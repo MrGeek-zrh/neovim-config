f6d59a3c37911a2a1032185ee110cc0695ad9e88
MrGeek-zrh <mrgeek-zrh@outlook.com>
2024-01-21 15:36:07 +0800

    Initial commit

---
+++ config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("t", "<c-h>", "<c-h>", nil)
+vim.keymap.set("t", "<c-j>", "<c-j>", nil)
+vim.keymap.set("t", "<c-k>", "<c-k>", nil)
+vim.keymap.set("t", "<c-l>", "<c-l>", nil)
